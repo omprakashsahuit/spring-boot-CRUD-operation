57680dbaa493f05f7d655fbb95d84a1019bcffd0
Om Prakash Sahu <omprakashsahuit@gmail.com>
2022-06-16 00:11:45 +0530

    first commit

---
+++ spring-boot-series/src/main/java/com/series/springboot/SpringBootSeriesApplication.java
@@ -0,0 +1,13 @@
+package com.series.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSeriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSeriesApplication.class, args);
+	}
+
+}
---
+++ spring-boot-series/src/test/java/com/series/springboot/SpringBootSeriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.series.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSeriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
